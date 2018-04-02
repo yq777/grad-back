a6e64020a8566d460e7671f0aa9695b4dfaf8d40
yq <1045046593@qq.com>
2018-04-02 16:54:44 +0800

    init

---
+++ src/main/java/com/grad/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.grad.dao;
+
+import com.grad.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
